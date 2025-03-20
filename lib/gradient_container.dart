0b9d33de57b2f3e1999e99cf9f773f07ae56c7e1
KayabaZeref <duyphuong2000.dpp@gmail.com>
2025-03-20 16:55:02 +0700

    Refactor GradientContainer to use variables for alignment in gradient background

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:first_app/style_text.dart';
 
+// final or const startAlignment = Alignment.topCenter;
+// final or const endAlignment = Alignment.bottomCenter;
+
+var startAlignment = Alignment.topCenter;
+var endAlignment = Alignment.bottomCenter;
+
 // StatelessWidget is a widget that need when you define your own widget
 class GradientContainer extends StatelessWidget {
   // super(key: key). First key call from StatelessWidget(super). Second key call from Container
@@ -11,12 +17,12 @@ class GradientContainer extends StatelessWidget {
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
           // begin: first coordinate of the gradient
-          begin: Alignment.topCenter,
+          begin: startAlignment,
           // end: second coordinate of the gradient
-          end: Alignment.bottomCenter,
+          end: endAlignment,
           colors: [Color(0xFF6A1B9A), Color(0xFF4527A0)],
         ),
       ),
